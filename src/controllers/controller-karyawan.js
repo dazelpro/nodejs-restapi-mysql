9066ffe322f07915b2a7ac753fca7c79fd33b74f
dazelpro <dazelpro@gmail.com>
2020-12-02 08:57:51 +0700

    Code 1.4

--- src/controllers/controller-karyawan.js
+++ src/controllers/controller-karyawan.js
@@ -44,5 +44,73 @@ module.exports ={
             });
             connection.release();
         })
+    },
+    addDataKaryawan(req,res){
+        let data = {
+            karyawan_nama : req.body.nama,
+            karyawan_umur : req.body.umur,
+            karyawan_alamat : req.body.alamat,
+            karyawan_jabatan : req.body.jabatan
+        }
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                INSERT INTO tabel_karyawan SET ?;
+                `
+            , [data],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil tambah data!',
+                });
+            });
+            connection.release();
+        })
+    },
+    editDataKaryawan(req,res){
+        let dataEdit = {
+            karyawan_nama : req.body.nama,
+            karyawan_umur : req.body.umur,
+            karyawan_alamat : req.body.alamat,
+            karyawan_jabatan : req.body.jabatan
+        }
+        let id = req.body.id
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                UPDATE tabel_karyawan SET ? WHERE karyawan_id = ?;
+                `
+            , [dataEdit, id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil edit data!',
+                });
+            });
+            connection.release();
+        })
+    },
+    deleteDataKaryawan(req,res){
+        let id = req.body.id
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+            connection.query(
+                `
+                DELETE FROM tabel_karyawan WHERE karyawan_id = ?;
+                `
+            , [id],
+            function (error, results) {
+                if(error) throw error;  
+                res.send({ 
+                    success: true, 
+                    message: 'Berhasil hapus data!'
+                });
+            });
+            connection.release();
+        })
     }
 }
--- src/routes/route-karyawan.js
+++ src/routes/route-karyawan.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const { karyawan } = require('../controllers');
 
+// GET localhost:8080/karyawan => Ambil data semua karyawan
 router.get('/karyawan', karyawan.getDataKaryawan);
+
+// GET localhost:8080/karyawan/2 => Ambil data semua karyawan berdasarkan id = 2
 router.get('/karyawan/:id', karyawan.getDataKaryawanByID);
+
+// POST localhost:8080/karyawan/add => Tambah data karyawan ke database
+router.post('/karyawan/add', karyawan.addDataKaryawan);
+
+// POST localhost:8080/karyawan/2 => Edit data karyawan
+router.post('/karyawan/edit', karyawan.editDataKaryawan);
+
+// POST localhost:8080/karyawan/delete => Delete data karyawan
+router.post('/karyawan/delete/', karyawan.deleteDataKaryawan);
 
 module.exports = router;
